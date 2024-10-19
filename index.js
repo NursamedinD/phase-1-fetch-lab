6fc8c1ee5e8e6ddd653677d960c576b4608b6bd2
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-10-19 17:17:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch ('https://anapioficeandfire.com/api/books')
+  .then (response => {
+    if (!response.ok) {
+      throw new Error('Something went wrong with the request');
+    }
+    return response.json();
+  })
+  .then(function(data) {
+    renderBooks(data);
+  })
+  .catch(function(error) {
+    console.error('There was a problem with the fetch:', error);
+  });
 }
 
 function renderBooks(books) {
